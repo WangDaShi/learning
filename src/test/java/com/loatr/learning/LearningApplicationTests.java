7591a945ab6909d5a79f89c42110a87fdfef6733
Loatr99 <1530805767@qq.com>
2020-09-06 16:39:59 +0800

    临时存放在这个工程里

--- src/test/java/com/loatr/learning/LearningApplicationTests.java
+++ src/test/java/com/loatr/learning/LearningApplicationTests.java
@@ -3,11 +3,31 @@ package com.loatr.learning;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Objects;
+
 @SpringBootTest
 class LearningApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
+	public static void main(String[] args) {
+		String str = null;
+		String s = str + "";
+		System.out.println(s);
+	}
+
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null || getClass() != obj.getClass())
+			return false;
+		UnitVisibleEmployee2 other = (UnitVisibleEmployee2) obj;
+		if(!Objects.equals(unitId,other.unitId)){
+			return false;
+		}
+		return Objects.equals(unitVisibleEmployees ,other.unitVisibleEmployees);
+	}
 }
---
+++ src/test/java/com/loatr/learning/SimpleTest.java
@@ -0,0 +1,4 @@
+package com.loatr.learning;
+
+public class SimpleTest {
+}
