e917a62706818f2e9ea9c8c4d2ac4726ed8ca951
Loatr99 <1530805767@qq.com>
2020-11-14 22:31:41 +0800

    配置了log

--- src/main/java/com/loatr/learning/controller/RestApiController.java
+++ src/main/java/com/loatr/learning/controller/RestApiController.java
@@ -1,24 +1,39 @@
 package com.loatr.learning.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import com.loatr.learning.model.Note;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class RestApiController {
 
     private Random r = new Random();
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @GetMapping("random/{max}")
     public Integer getRandomNumber(@PathVariable("max") int max){
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return r.nextInt(max);
     }
 
-    @GetMapping("test")
-    public String getTest(){
-        return "test";
+    @PostMapping("post")
+    public String receiveMsg1(@RequestBody Note note){
+        logger.debug("id:{},content:{}",note.getId(),note.getContent());
+        return "success";
+    }
+
+    @PostMapping("note/add")
+    public String receiveMsg2(@RequestParam String content){
+        logger.debug("{}",content);
+        return content;
     }
 
 }
---
+++ src/main/java/com/loatr/learning/model/Note.java
@@ -0,0 +1,13 @@
+package com.loatr.learning.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Note {
+
+    private String content;
+    private Integer id;
+
+}
